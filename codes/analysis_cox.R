e6d8c6efae5b8d2a2431a39fb6e9a84e3afc561d
Iwaki <iwakih2@NIA2-1801778980.local>
2018-06-20 09:46:22 -0400

    test cox analysis- working

--- codes/analysis_cox.R
+++ codes/analysis_cox.R
@@ -1,90 +1,84 @@
 # Cox model for GWAS
 
-rm(list=ls())
-Sys.setlocale('LC_ALL', locale='C')
-# setwd("/Users/iwakih2/Downloads/META_new")
-
-
 library(data.table)
 library(dplyr)
-library(tableone)
 library(zoo)
 library(survival)
+library(parallel)
+# Read data
+ltg_all=fread('../PhenoFile.csv') %>% data.frame
+names(ltg_all)
 
-
-##### FACOTORS ######################################################################################
-Covs = c('FEMALE', 'YEARSEDUC', 'AGEatBL', 'BLDfDIAG','TSTART', 'DOPA', 'AGONIST')
-BasicT = c('AAO', 'FAMILY_HISTORY')
-BinomT = c('HYPOSMIA', 'DEMENTIA', 'MOTORFLUX', 'DYSKINESIAS', 'DEPR', 'RL', 'CONST', 'RBD', 'SLEEP', 'INS', 'HY3')
-# DEATH often happened after the observation period so should be analyzed separately. 
-predictors <- c("grsZ","NeuroX_dbSNP_rs114138760_C","exm106217_C","exm106220_A","NeuroX_rs2230288_A",
-                "NeuroX_rs823118_C","NeuroX_rs10797576_T","NeuroX_rs6430538_T","NeuroX_rs1955337_T",
-                "NeuroX_rs12637471_A","NeuroX_dbSNP_rs34884217_replciate_1_G","NeuroX_rs34311866_G",
-                "NeuroX_rs11724635_C","exm.rs6812193_T","NeuroX_rs356181_T","NeuroX_rs3910105_C",
-                "exm535099_T","NeuroX_dbSNP_rs115462410_T","NeuroX_rs199347_C","NeuroX_rs591323_A",
-                "NeuroX_dbSNP_rs118117788_T","NeuroX_rs329648_T","NeuroX_rs76904798_T","exm994671_A",
-                "NeuroX_rs11060180_G","NeuroX_rs11158026_T","NeuroX_rs2414739_G",
-                "NeuroX_dbSNP_rs14235_replciate_1_A","exm.rs11868035_A","NeuroX_rs17649553_T",
-                "NeuroX_rs12456492_G","NeuroX_rs55785911_A")
+# Factors
 PCs = c("PC1", "PC2")
-######################################################################################################
-ltg_all=fread('../data/LTGall.csv') %>% data.frame %>%
-  filter(STUDY_NAME != "Predict") %>% 
-  rename(COHORT = STUDY_NAME) %>% 
-  mutate(COHORT= case_when(
-    COHORT == "Coriell" ~ "NET-PD_LS1",
-    COHORT == "SCOPA" ~ "PROPARK",
-    COHORT == "PreCEPT" ~ "PRECEPT",
-    COHORT == "ParkFit" ~ "PARKFIT",
-    COHORT == "ParkWest" ~ "PARKWEST",
-    COHORT == "Udall" ~ "UDALL",
-    TRUE ~ COHORT
-  )) %>% 
-  select(c("COHORT", "ID", Covs, BasicT, BinomT, PCs, predictors))
-
-Oslo = HYUPDRSg %>% 
-  mutate(COHORT = "Oslo") %>% 
-  select(names(ltg_all))
-
-data_all = rbind(ltg_all, Oslo) %>% 
-  arrange(COHORT)
-
+COVs = c("ID", 'FEMALE', 'YEARSEDUC', "FAMILY_HISTORY", 'AAO', "BLDfDIAG",  'DOPA', 'AGONIST', "TSTART")
+OUTCOMEs = c('HYPOSMIA', 'DEMENTIA', 'MOTORFLUX', 'DYSKINESIAS', 'DEPR', 'RL', 'CONST', 'RBD', 'SLEEP', 'INS', 'HY3')
+COHORTs = unique(ltg_all$COHORT)
 N_VARS = 2 + length(c(Covs, BasicT, BinomT, PCs))
-
+PREDICTORs = paste("X", 1:12, sep = "")
 #######################################################################
 # models specifications
-COHORTS = unique(data_all$COHORT)
-COHORTS
-MODELS = data.frame(
-  COHORT = COHORTS,
+COHORTs
+MODELs = data.frame(
+  COHORT = COHORTs,
   MODEL = c(
+    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2                  + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
+    "SurvObj1 ~ PREDICTOR + PC1 + PC2                  + FEMALE             + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2                  + FEMALE                              + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
     "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR + PC1 + PC2                  + FEMALE             + FAMILY_HISTORY + AAO + BLDfDIAG",
-    "SurvObj1 ~ PREDICTOR             + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG"
-  )
+    "SurvObj1 ~ PREDICTOR + PC1 + PC2 + DOPA + AGONIST + FEMALE + YEARSEDUC + FAMILY_HISTORY + AAO + BLDfDIAG"
+    )
 )
+MODELs
+
+temp1 = ltg_all %>% filter(COHORT == "PDBP") %>% distinct(ID, PID)
 # Another way is to replace missing covariates with the same value like "MISS"
-GRID = expand.grid(1:length(BinomT), 1:length(predictors))
+
+# For test
+ltg_all = ltg_all %>% mutate(PC1 = rnorm(nrow(ltg_all), 0, 1), 
+                             PC2 = rnorm(nrow(ltg_all), 0, 1),
+                             X1 = rnorm(nrow(ltg_all), 0, 1),
+                             X2 = rnorm(nrow(ltg_all), 0, 1),
+                             X3 = rnorm(nrow(ltg_all), 0, 1),
+                             X4 = rnorm(nrow(ltg_all), 0, 1),
+                             X5 = rnorm(nrow(ltg_all), 0, 1),
+                             X6 = rnorm(nrow(ltg_all), 0, 1),
+                             X7 = rnorm(nrow(ltg_all), 0, 1),
+                             X8 = rnorm(nrow(ltg_all), 0, 1),
+                             X9 = rnorm(nrow(ltg_all), 0, 1),
+                             X10 = rnorm(nrow(ltg_all), 0, 1),
+                             X11 = rnorm(nrow(ltg_all), 0, 1),
+                             X12 = rnorm(nrow(ltg_all), 0, 1),
+                             X13 = rnorm(nrow(ltg_all), 0, 1),
+                             X14 = rnorm(nrow(ltg_all), 0, 1),
+                             X15 = rnorm(nrow(ltg_all), 0, 1),
+                             X16 = rnorm(nrow(ltg_all), 0, 1),
+                             X17 = rnorm(nrow(ltg_all), 0, 1),
+                             X18 = rnorm(nrow(ltg_all), 0, 1),
+                             X19 = rnorm(nrow(ltg_all), 0, 1),
+                             X20 = rnorm(nrow(ltg_all), 0, 1),
+                             X21 = rnorm(nrow(ltg_all), 0, 1),
+                             X22 = rnorm(nrow(ltg_all), 0, 1),
+                             X23 = rnorm(nrow(ltg_all), 0, 1),
+                             X24 = rnorm(nrow(ltg_all), 0, 1),
+                             X25 = rnorm(nrow(ltg_all), 0, 1),)
+
+# Survival Analaysis
+GRID = expand.grid(1:length(OUTCOMEs), 1:length(PREDICTORs))
 cox.stats.func <- function(x){
-  STUDY = COHORTS[i]
-  OUTCOME = BinomT[GRID[x,1]]
-  PREDICTOR = predictors[GRID[x,2]]
+  STUDY = COHORTs[i]
+  OUTCOME = OUTCOMEs[GRID[x,1]]
+  PREDICTOR = PREDICTORs[GRID[x,2]]
   # print(paste(i, x, STUDY, OUTCOME, PREDICTOR))
-  ltg = subset(data_all, COHORT == STUDY)
-  ltg$OUTCOME <- ltg[,OUTCOME]
+  ltg = ltg_all %>% filter(COHORT == STUDY)
+  ltg$OUTCOME = ltg[,OUTCOME]
   ltg$PREDICTOR = ltg[,PREDICTOR]
-  ltg = ltg %>% select(-one_of(predictors))
-  
+  ltg = ltg %>% select(OUTCOME, PREDICTOR, COVs, PCs)
   time2follow = ltg %>%
     arrange(TSTART) %>%
     filter(!is.na(OUTCOME)) %>%
@@ -117,7 +111,7 @@ cox.stats.func <- function(x){
     sumstat <- cbind(STUDY, OUTCOME, PREDICTOR, "NoVar", "NoVar", "NoVar", "NoVar", "NoVar")
   }else{
     cohort$SurvObj1 = with(cohort, Surv(TSTART, TSTOP, OUTCOME == 1))
-    testCox = try(coxph(eval(parse(text = paste(MODELS[i,2]))), data = cohort),silent = T)
+    testCox = try(coxph(eval(parse(text = paste(MODELs[i,2]))), data = cohort),silent = T)
     # Some can't converge by REML so use alternative method
     if(class(testCox)[1]=='try-error'){
       sumstat <- cbind(STUDY, OUTCOME, PREDICTOR, "OverFlow", "OverFlow", "OverFlow", "OverFlow", "OverFlow")
@@ -131,15 +125,13 @@ cox.stats.func <- function(x){
       sumstat <- cbind(STUDY, OUTCOME, PREDICTOR, betas, se, zval, pval, NinSet)
     }
   }
-  sumstat
+  sumstat[1,]
 }
 
+t <- proc.time()
 temp = mclapply(1:nrow(GRID), cox.stats.func, mc.cores = 2)
 temp2 = do.call(rbind, temp)
+proc.time() - t
 
-# t <- proc.time()
-# invisible(mclapply(1:nrow(GRID), cox.stats.func, mc.cores = 2))
-# duration3 <- (proc.time() - t)[3]
-# t <- proc.time()
-# invisible(lapply(1:nrow(GRID), cox.stats.func))
-# duration2 <- (proc.time() - t)[3]
+
+rm(list=ls())
